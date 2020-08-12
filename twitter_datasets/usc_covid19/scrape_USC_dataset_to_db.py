76264bb0d072795a5230c723c02898b6bbb77c1f
lynneeai <lynneeai@hotmail.com>
2020-08-12 01:22:43 -0700

    hydrate

--- twitter_datasets/usc_covid19/scrape_USC_dataset_to_db.py
+++ twitter_datasets/usc_covid19/scrape_USC_dataset_to_db.py
@@ -1,8 +1,9 @@
-'''Solve import issue'''
+"""Solve import issue"""
 import os
 import sys
+
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
-project_root_dir = f'{current_file_dir}/../../'
+project_root_dir = f"{current_file_dir}/../../"
 sys.path.append(current_file_dir)
 sys.path.append(project_root_dir)
 
@@ -11,107 +12,112 @@ import tweepy
 import traceback
 from tqdm import tqdm
 from usc_scraper import USC_SCRAPER
-from utils.sqlite_utils import TABLE, create_table, clear_table, drop_table, batch_insert
+from utils.sqlite_utils import (
+    TABLE,
+    create_table,
+    clear_table,
+    drop_table,
+    batch_insert,
+)
 from utils.all_utils import program_sleep
 
-DB_FILE = f'{current_file_dir}/usc_twitter.db'
+DB_FILE = f"{current_file_dir}/usc_twitter.db"
 CLEAR_TABLE = False
 DROP_TABLE = False
 
-'''Tables Configs'''
+"""Tables Configs"""
 # -------covid19_tweets-------
-COVID19_TWEETS = TABLE('covid19_tweets', ['tweet_id', 'full_text', 'created_at', 'language', 'hashtags_str', 'mentions_str', 'favorite_count', 'retweet_count'])
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.tweet_id,       'TEXT PRIMARY KEY')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.full_text,      'TEXT NOT NULL')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.created_at,     'TEXT NOT NULL')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.language,       'TEXT')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.hashtags_str,   'TEXT')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.mentions_str,   'TEXT')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.favorite_count, 'INTEGER')
-COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.retweet_count,  'INTEGER')
-assert(len(COVID19_TWEETS.cols) == len(COVID19_TWEETS.cols_const))
+COVID19_TWEETS = TABLE("covid19_tweets", ["tweet_id", "full_text", "created_at", "language", "hashtags_str", "mentions_str", "favorite_count", "retweet_count",],)
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.tweet_id, "TEXT PRIMARY KEY")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.full_text, "TEXT NOT NULL")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.created_at, "TEXT NOT NULL")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.language, "TEXT")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.hashtags_str, "TEXT")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.mentions_str, "TEXT")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.favorite_count, "INTEGER")
+COVID19_TWEETS.add_constraint(COVID19_TWEETS.cols.retweet_count, "INTEGER")
+assert len(COVID19_TWEETS.cols) == len(COVID19_TWEETS.cols_const)
 
 # -------images-------
-IMAGES = TABLE('images', ['tweet_id', 'media_url'])
-IMAGES.add_constraint(IMAGES.cols.tweet_id,  'TEXT NOT NULL')
-IMAGES.add_constraint(IMAGES.cols.media_url, 'TEXT NOT NULL')
+IMAGES = TABLE("images", ["tweet_id", "media_url"])
+IMAGES.add_constraint(IMAGES.cols.tweet_id, "TEXT NOT NULL")
+IMAGES.add_constraint(IMAGES.cols.media_url, "TEXT NOT NULL")
 IMAGES.add_primary_key((IMAGES.cols.tweet_id, IMAGES.cols.media_url))
-IMAGES.add_foreign_key((IMAGES.cols.tweet_id, f'{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})'))
-assert(len(IMAGES.cols) == len(IMAGES.cols_const))
+IMAGES.add_foreign_key((IMAGES.cols.tweet_id, f"{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})"))
+assert len(IMAGES.cols) == len(IMAGES.cols_const)
 
 # -------videos-------
-VIDEOS = TABLE('videos', ['tweet_id', 'media_url'])
-VIDEOS.add_constraint(VIDEOS.cols.tweet_id,  'TEXT NOT NULL')
-VIDEOS.add_constraint(VIDEOS.cols.media_url, 'TEXT NOT NULL')
+VIDEOS = TABLE("videos", ["tweet_id", "media_url"])
+VIDEOS.add_constraint(VIDEOS.cols.tweet_id, "TEXT NOT NULL")
+VIDEOS.add_constraint(VIDEOS.cols.media_url, "TEXT NOT NULL")
 VIDEOS.add_primary_key((VIDEOS.cols.tweet_id, VIDEOS.cols.media_url))
-VIDEOS.add_foreign_key((VIDEOS.cols.tweet_id, f'{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})'))
-assert(len(VIDEOS.cols) == len(VIDEOS.cols_const))
+VIDEOS.add_foreign_key((VIDEOS.cols.tweet_id, f"{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})"))
+assert len(VIDEOS.cols) == len(VIDEOS.cols_const)
 
 # -------gifs-------
-GIFS = TABLE('gifs', ['tweet_id', 'media_url'])
-GIFS.add_constraint(GIFS.cols.tweet_id,  'TEXT NOT NULL')
-GIFS.add_constraint(GIFS.cols.media_url, 'TEXT NOT NULL')
+GIFS = TABLE("gifs", ["tweet_id", "media_url"])
+GIFS.add_constraint(GIFS.cols.tweet_id, "TEXT NOT NULL")
+GIFS.add_constraint(GIFS.cols.media_url, "TEXT NOT NULL")
 GIFS.add_primary_key((GIFS.cols.tweet_id, GIFS.cols.media_url))
-GIFS.add_foreign_key((GIFS.cols.tweet_id, f'{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})'))
-assert(len(GIFS.cols) == len(GIFS.cols_const))
+GIFS.add_foreign_key((GIFS.cols.tweet_id, f"{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})"))
+assert len(GIFS.cols) == len(GIFS.cols_const)
 
 # -------externals-------
-EXTERNALS = TABLE('externals', ['tweet_id', 'media_url'])
-EXTERNALS.add_constraint(EXTERNALS.cols.tweet_id,  'TEXT NOT NULL')
-EXTERNALS.add_constraint(EXTERNALS.cols.media_url, 'TEXT NOT NULL')
+EXTERNALS = TABLE("externals", ["tweet_id", "media_url"])
+EXTERNALS.add_constraint(EXTERNALS.cols.tweet_id, "TEXT NOT NULL")
+EXTERNALS.add_constraint(EXTERNALS.cols.media_url, "TEXT NOT NULL")
 EXTERNALS.add_primary_key((EXTERNALS.cols.tweet_id, EXTERNALS.cols.media_url))
-EXTERNALS.add_foreign_key((EXTERNALS.cols.tweet_id, f'{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})'))
-assert(len(EXTERNALS.cols) == len(EXTERNALS.cols_const))
+EXTERNALS.add_foreign_key((EXTERNALS.cols.tweet_id, f"{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})"))
+assert len(EXTERNALS.cols) == len(EXTERNALS.cols_const)
 
 # -------election2020_tweets-------
-ELECTION_TWEETS = TABLE('election2020_tweets', ['tweet_id'])
-ELECTION_TWEETS.add_constraint(ELECTION_TWEETS.cols.tweet_id, 'TEXT PRIMARY KEY')
-ELECTION_TWEETS.add_foreign_key((ELECTION_TWEETS.cols.tweet_id, f'{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})'))
-assert(len(ELECTION_TWEETS.cols) == len(ELECTION_TWEETS.cols_const))
+ELECTION_TWEETS = TABLE("election2020_tweets", ["tweet_id"])
+ELECTION_TWEETS.add_constraint(ELECTION_TWEETS.cols.tweet_id, "TEXT PRIMARY KEY")
+ELECTION_TWEETS.add_foreign_key((ELECTION_TWEETS.cols.tweet_id, f"{COVID19_TWEETS.name}({COVID19_TWEETS.cols.tweet_id})",))
+assert len(ELECTION_TWEETS.cols) == len(ELECTION_TWEETS.cols_const)
 
 if __name__ == "__main__":
 
-	'''db connection'''
-	CONN = sqlite3.connect(DB_FILE)
-	CUR = CONN.cursor()
+    """db connection"""
+    CONN = sqlite3.connect(DB_FILE)
+    CUR = CONN.cursor()
 
-	try:
-		'''create tables'''
-		if DROP_TABLE:
-			for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS, ELECTION_TWEETS]:
-				drop_table(t.name, CUR)
-				CONN.commit()
+    try:
+        """create tables"""
+        if DROP_TABLE:
+            for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS, ELECTION_TWEETS]:
+                drop_table(t.name, CUR)
+                CONN.commit()
 
-		for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS, ELECTION_TWEETS]:
-			create_table(table_name=t.name, cols_constraints_dict=t.cols_const, cur=CUR, primary_key=t.pk, foreign_keys=t.fks)
-			CONN.commit()
+        for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS, ELECTION_TWEETS]:
+            create_table(
+                table_name=t.name, cols_constraints_dict=t.cols_const, cur=CUR, primary_key=t.pk, foreign_keys=t.fks,
+            )
+            CONN.commit()
 
-		if CLEAR_TABLE:
-			for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS, ELECTION_TWEETS]:
-				clear_table(t.name, CUR)
-				CONN.commit()
+        if CLEAR_TABLE:
+            for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS, ELECTION_TWEETS]:
+                clear_table(t.name, CUR)
+                CONN.commit()
 
-		'''covid19 scraper'''
-		usc_scraper = USC_SCRAPER()
-		while usc_scraper.has_next_batch():
-			obj = usc_scraper.next_batch()
-			for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS]:
-				try:
-					batch_insert(t.name, t.cols_list, obj[t.name], CUR)
-					CONN.commit()
-				except Exception as e:
-					print(e)
+        """covid19 scraper"""
+        usc_scraper = USC_SCRAPER()
+        while usc_scraper.has_next_batch():
+            obj = usc_scraper.next_batch()
+            for t in [COVID19_TWEETS, IMAGES, VIDEOS, GIFS, EXTERNALS]:
+                try:
+                    batch_insert(t.name, t.cols_list, obj[t.name], CUR)
+                    CONN.commit()
+                except Exception as e:
+                    print(e)
 
-			if obj['limit_exceeded']:
-				print('Rate limit exceeded!')
-				program_sleep(900)
+            if obj["limit_exceeded"]:
+                print("Rate limit exceeded!")
+                program_sleep(900)
 
-	except:
-		print(traceback.format_exc())
-	
-	'''close db connection'''
-	CONN.commit()
-	CONN.close()
+    except:
+        print(traceback.format_exc())
 
-
-
+    """close db connection"""
+    CONN.commit()
+    CONN.close()
--- twitter_datasets/usc_covid19/usc_scraper.py
+++ twitter_datasets/usc_covid19/usc_scraper.py
@@ -1,8 +1,9 @@
-'''Solve import issue'''
+"""Solve import issue"""
 import os
 import sys
+
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
-project_root_dir = f'{current_file_dir}/../../'
+project_root_dir = f"{current_file_dir}/../../"
 sys.path.append(current_file_dir)
 sys.path.append(project_root_dir)
 
@@ -13,89 +14,103 @@ from tqdm import tqdm
 from twitter_datasets.utils.twitter_scraper_utils import get_single_tweet_by_id
 from utils.all_utils import write_to_log
 
-ID_FILES_ROOT = './USC_dataset/'
+ID_FILES_ROOT = "./USC_dataset/"
+
 
 class USC_SCRAPER:
-	def __init__(self):
-		self.id_files = []
-		for folder in os.listdir(ID_FILES_ROOT):
-			month_root = f'{ID_FILES_ROOT}{folder}/'
-			for id_file in os.listdir(month_root):
-				self.id_files.append(f'{month_root}{id_file}')
-		self.id_files.sort(reverse=True)
-		self.next_file_ptr = 14
-		self.current_id_file = ''
+    def __init__(self):
+        self.id_files = []
+        for folder in os.listdir(ID_FILES_ROOT):
+            month_root = f"{ID_FILES_ROOT}{folder}/"
+            for id_file in os.listdir(month_root):
+                self.id_files.append(f"{month_root}{id_file}")
+        self.id_files.sort(reverse=True)
+        self.next_file_ptr = 14
+        self.current_id_file = ""
 
-		self.cached_ids = []
-		self.next_cached_id_ptr = 29929
-		self.log_file = f'../logs/usc_scraper_{datetime.now().strftime("%Y%m%d_%H:%M:%S")}.txt'
+        self.cached_ids = []
+        self.next_cached_id_ptr = 29929
+        self.log_file = f'../logs/usc_scraper_{datetime.now().strftime("%Y%m%d_%H:%M:%S")}.txt'
 
-		self.is_first_batch = True
+        self.is_first_batch = True
 
-	def next_batch(self):
-		if self.next_cached_id_ptr >= len(self.cached_ids):
-			self.cached_ids = []
-			self.next_cached_id_ptr = self.next_cached_id_ptr if self.is_first_batch else 0
-			self.current_id_file = self.id_files[self.next_file_ptr]
-			with open(self.current_id_file, 'r') as infile:
-				for line in infile:
-					tweet_id = line.strip()
-					self.cached_ids.append(tweet_id)
-			
-			write_to_log(self.log_file, f'-------[{datetime.now().strftime("%H:%M:%S")}] Starting processing file with idx {self.next_file_ptr}: {self.current_id_file}-------')
-			self.next_file_ptr += 1
-			self.is_first_batch = False
+    def next_batch(self):
+        if self.next_cached_id_ptr >= len(self.cached_ids):
+            self.cached_ids = []
+            self.next_cached_id_ptr = self.next_cached_id_ptr if self.is_first_batch else 0
+            self.current_id_file = self.id_files[self.next_file_ptr]
+            with open(self.current_id_file, "r") as infile:
+                for line in infile:
+                    tweet_id = line.strip()
+                    self.cached_ids.append(tweet_id)
 
-		print(f'Getting Tweets in {self.current_id_file}...')
-		
-		limit_exceeded = False
-		covid19_tweets_todb = []
-		images_todb = []
-		videos_todb = []
-		gifs_todb = []
-		externals_todb = []
-		
-		trange = tqdm(range(self.next_cached_id_ptr, len(self.cached_ids)))
-		for idx in trange:
-			tweet_id = self.cached_ids[idx]
-			try:
-				obj = get_single_tweet_by_id(tweet_id)
-				if obj:
-					covid19_tweets_todb.append([obj['tweet_id'], obj['full_text'], obj['created_at'], obj['language'], 
-												obj['hashtags_str'], obj['mentions_str'], obj['favorite_count'], obj['retweet_count']])
-					for media in obj['all_media_urls']:
-						if media['media_type'] == 'photo':
-							images_todb.append([tweet_id, media['media_url']])
-						elif media['media_type'] == 'video':
-							videos_todb.append([tweet_id, media['media_url']])
-						elif media['media_type'] == 'animated_gif':
-							gifs_todb.append([tweet_id, media['media_url']])
-						elif media['media_type'] == 'other':
-							externals_todb.append([tweet_id, media['media_url']])
+            write_to_log(
+                self.log_file, f'-------[{datetime.now().strftime("%H:%M:%S")}] Starting processing file with idx {self.next_file_ptr}: {self.current_id_file}-------',
+            )
+            self.next_file_ptr += 1
+            self.is_first_batch = False
 
-			except Exception as e:
-				try:
-					error_code = str(e).split('}]:')[0]
-					error_code = error_code.split('\'code\': ')[1]
-					if error_code == '88':
-						limit_exceeded = True
-						self.next_cached_id_ptr = idx
-						write_to_log(self.log_file, f'**[{datetime.now().strftime("%H:%M:%S")}]** Finished {idx} tweets in file {self.next_file_ptr - 1}! Next tweet idx: {self.next_cached_id_ptr}')
-						trange.close()
-						break
-					else:
-						write_to_log(self.log_file, f'{e}')
-				except Exception as e:
-					write_to_log(self.log_file, f'{e}')
-		
-		trange.close()
-		time.sleep(1)
-		if not limit_exceeded:
-			self.next_cached_id_ptr = len(self.cached_ids)
-			write_to_log(self.log_file, f'-------[{datetime.now().strftime("%H:%M:%S")}] Finished processing file with idx {self.next_file_ptr - 1}: {self.current_id_file}-------\n')
+        print(f"Getting Tweets in {self.current_id_file}...")
 
-		return {'limit_exceeded':limit_exceeded, 'covid19_tweets':covid19_tweets_todb, 'images':images_todb, 'videos':videos_todb, 
-				'gifs':gifs_todb, 'externals':externals_todb}
+        limit_exceeded = False
+        covid19_tweets_todb = []
+        images_todb = []
+        videos_todb = []
+        gifs_todb = []
+        externals_todb = []
 
-	def has_next_batch(self):
-		return (self.next_file_ptr < len(self.id_files)) or (self.next_cached_id_ptr < len(self.cached_ids))
+        trange = tqdm(range(self.next_cached_id_ptr, len(self.cached_ids)))
+        for idx in trange:
+            tweet_id = self.cached_ids[idx]
+            try:
+                obj = get_single_tweet_by_id(tweet_id)
+                if obj:
+                    covid19_tweets_todb.append(
+                        [obj["tweet_id"], obj["full_text"], obj["created_at"], obj["language"], obj["hashtags_str"], obj["mentions_str"], obj["favorite_count"], obj["retweet_count"],]
+                    )
+                    for media in obj["all_media_urls"]:
+                        if media["media_type"] == "photo":
+                            images_todb.append([tweet_id, media["media_url"]])
+                        elif media["media_type"] == "video":
+                            videos_todb.append([tweet_id, media["media_url"]])
+                        elif media["media_type"] == "animated_gif":
+                            gifs_todb.append([tweet_id, media["media_url"]])
+                        elif media["media_type"] == "other":
+                            externals_todb.append([tweet_id, media["media_url"]])
+
+            except Exception as e:
+                try:
+                    error_code = str(e).split("}]:")[0]
+                    error_code = error_code.split("'code': ")[1]
+                    if error_code == "88":
+                        limit_exceeded = True
+                        self.next_cached_id_ptr = idx
+                        write_to_log(
+                            self.log_file, f'**[{datetime.now().strftime("%H:%M:%S")}]** Finished {idx} tweets in file {self.next_file_ptr - 1}! Next tweet idx: {self.next_cached_id_ptr}',
+                        )
+                        trange.close()
+                        break
+                    else:
+                        write_to_log(self.log_file, f"{e}")
+                except Exception as e:
+                    write_to_log(self.log_file, f"{e}")
+
+        trange.close()
+        time.sleep(1)
+        if not limit_exceeded:
+            self.next_cached_id_ptr = len(self.cached_ids)
+            write_to_log(
+                self.log_file, f'-------[{datetime.now().strftime("%H:%M:%S")}] Finished processing file with idx {self.next_file_ptr - 1}: {self.current_id_file}-------\n',
+            )
+
+        return {
+            "limit_exceeded": limit_exceeded,
+            "covid19_tweets": covid19_tweets_todb,
+            "images": images_todb,
+            "videos": videos_todb,
+            "gifs": gifs_todb,
+            "externals": externals_todb,
+        }
+
+    def has_next_batch(self):
+        return (self.next_file_ptr < len(self.id_files)) or (self.next_cached_id_ptr < len(self.cached_ids))
